7286e9a4d2bf122767b22d427b05e15c0233c43b
Grigori007 <greg.kdev05@gmail.com>
2025-09-15 22:20:22 +0200

    EXR improved

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import { initBaseScene, initCubeScene, disposeModels, disposeWholeScene } from "./cube";
 import { initExrGallery, getExrFileName } from "./exrGallery";
 
-// initExrGallery();
-// const testExrFileName = getExrFileName();
+//initExrGallery();
+//const testExrFileName = getExrFileName();
 
 const CubeConfigs = {
     Gold: {
         mainTexturePath: "https://cdn.jsdelivr.net/gh/Grigori007/Grigori007.github.io@wip3/src/assets/textures/v3/simple_gold_texture.png",
         logoPath: "https://cdn.jsdelivr.net/gh/Grigori007/Grigori007.github.io@wip3/src/assets/textures/v3/Zloto.png",
         roughTexturePath: "https://cdn.jsdelivr.net/gh/Grigori007/Grigori007.github.io@wip3/src/assets/textures/v3/simple_rough_texture.png",
-        hdrPath: "https://cdn.jsdelivr.net/gh/Grigori007/Grigori007.github.io@wip3/src/assets/textures/v2/GSG_PRO_STUDIOS_METAL_040_sm(2).exr",
+        hdrPath: "https://cdn.jsdelivr.net/gh/Grigori007/Grigori007.github.io@wip3/src/assets/textures/v2/GSG_PRO_STUDIOS_METAL_040_v2_sm.exr",
         metalness: 0.99,
         roughness: 0.7,
         backgroundColor: 0x192134,
@@ -27,7 +27,7 @@ const CubeConfigs = {
         metalness: 0.99,
         roughness: 0.7,
         backgroundColor: 0x192134,
-        enableAnimation: false,
+        enableAnimation: true,
         showAnimationControls: false,
         rednerLogoOnBothSides: true,
         envLightXRotationInPiRadians: 1.8,
